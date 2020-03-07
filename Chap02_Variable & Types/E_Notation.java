870a66f4f8c65fed3c33b72509a61179d9647cf8
yoondw <caandmaple@naver.com>
2020-03-07 17:55:59 +0900

    Chap02

---
+++ Chap02_Variable & Types/E_Notation.java
@@ -0,0 +1,22 @@
+class E_Notation {
+    public static void main(String[] args) {
+        double e1 = 1.2e-3; // 1.2 x 10^(-3)
+        double e2 = 1.2e+3; // 1.2 x 10^3
+
+        int num1 = 0xA0E; // 16진수
+        int num2 = 0752; // 8진수
+
+        System.out.println(e1);
+        System.out.println(e2);
+        System.out.println(num1);
+        System.out.println(num2);
+
+        char ch1 = 'A';
+        char ch2 = '한';
+        char ch3 = 0x3091;
+
+        System.out.println(ch1);
+        System.out.println(ch2);
+        System.out.println(ch3);
+    }
+}
---
+++ Chap02_Variable & Types/UseVariable.java
@@ -0,0 +1,17 @@
+class UseVariable {
+    public static void main(String[] args) {
+        int num1;
+        num1 = 10;
+        int num2 = 20;
+
+        int num3 = num1 + num2;
+        System.out.println(num1 + " + " + num2 + " + " + num3);
+
+        double db1, db2, result;
+        db1 = 1.0000000001;
+        db2 = 2.0000000001;
+        result = db1 + db2;
+
+        System.out.println(result);
+    }
+}
